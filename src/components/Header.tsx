1c145db133d10ad6d21e0d901cccb8fb663ad4fb
Pedro Germano <pedrogermano232@gmail.com>
2025-12-01 09:47:33 -0300

    feat: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header>
+      <img src="./logo.svg" alt="Logo" />
+      <a className="btn-primary" href="#">
+        Premium
+      </a>
+    </header>
+  );
+};
+
+// https://www.youtube.com/watch?v=WnQ6WrffEok&t=3600s
