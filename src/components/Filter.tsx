64daa6c30b02fbd692e9f67671500a3e9074c6af
Pedro Germano <pedrogermano232@gmail.com>
2025-12-05 06:36:29 -0300

    feat: component filter create

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,17 +1,24 @@
+import { Search } from "lucide-react";
+
 export function Filter() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center mt-10">
       <h1 className="text-2xl font-bold text-[#00A2F7]">
         Busque o conteúdo que deseja assitir hoje
       </h1>
       <p className="text-base font-medium  text-[#09A3FB]">
         Conteúdo exclusivo todos os dias
       </p>
-      <input
-        className="bg-white w-[615px] h-[45px] mt-4 p-2 rounded-lg border border-[#f2f2f2] outline-0"
-        type="text"
-        placeholder="Curso de react"
-      />
+      <div className="flex gap-3.5 items-center mt-2">
+        <input
+          className="bg-white w-[615px] h-[45px] p-2 rounded-lg border border-[#f2f2f2] outline-0"
+          type="text"
+          placeholder="Curso de react"
+        />
+        <div className="flex justify-center items-center rounded-sm h-[51px] w-[45px] bg-[#E1F4FF]  ">
+          <Search className=" text-[#00a2f7] " size={16} />
+        </div>
+      </div>
     </div>
   );
 }
